b40df7a80b5af23430e6cf4c7124027d37fea566
Manuel Schnitzer <mschnitzer@suse.de>
2015-10-12 12:35:34 +0200

    Replace tabs with spaces

--- lib/api.rb
+++ lib/api.rb
@@ -5,31 +5,31 @@ require_relative 'exceptions'
 class API
   
   def initialize(server, path, use_ssl)
-  	@server = server
-  	@path = path
-  	@use_ssl = use_ssl
+    @server = server
+    @path = path
+    @use_ssl = use_ssl
   end
 
   def server_version
-  	uri = build_uri("/")
-  	res = Net::HTTP.get_response(uri)
+    uri = build_uri("/")
+    res = Net::HTTP.get_response(uri)
 
-  	if !valid_response?(res.body)
-  	  raise InvalidAPIResponse.new
-  	end
+    if !valid_response?(res.body)
+      raise InvalidAPIResponse.new
+    end
 
-  	parsed_response = JSON.parse(res.body)
-  	parsed_response["api_version"]
+    parsed_response = JSON.parse(res.body)
+    parsed_response["api_version"]
   end
 
   def build_uri(api)
-  	URI(build_url(api))
+    URI(build_url(api))
   end
 
   def build_url(api)
-   	if @use_ssl == true
-  	  request_method = "https://"
-  	else
+    if @use_ssl == true
+      request_method = "https://"
+    else
       request_method = "http://"
     end
 
@@ -37,13 +37,13 @@ class API
   end
 
   def valid_response?(response)
-  	begin
-  	  JSON.parse(response)
-  	rescue JSON::ParserError => e
-  	  return false
-  	end
+    begin
+      JSON.parse(response)
+    rescue JSON::ParserError => e
+      return false
+    end
 
-  	true
+    true
   end
 
 end
--- spec/unit/api_spec.rb
+++ spec/unit/api_spec.rb
@@ -3,20 +3,20 @@ require_relative '../../lib/api'
 describe "API" do
 
   before(:all) do
-  	@api = API.new("samp-plugins.net", "/api", true)
+    @api = API.new("samp-plugins.net", "/api", true)
   end
 
   it "builds the URL for an API request" do
-  	expect(@api.build_url("/")).to eq("https://samp-plugins.net/api/")
+    expect(@api.build_url("/")).to eq("https://samp-plugins.net/api/")
   end
 
   it "returns the correct API version" do
-  	expect(@api.server_version).to eq("1.0.0")
+    expect(@api.server_version).to eq("1.0.0")
   end
 
   it "checks for the correctness of the API server response" do
-  	expect(@api.valid_response?("ABC")).to be(false)
-  	expect(@api.valid_response?('{ "name": "mschnitzer" }')).to be(true)
+    expect(@api.valid_response?("ABC")).to be(false)
+    expect(@api.valid_response?('{ "name": "mschnitzer" }')).to be(true)
   end
 
 end
